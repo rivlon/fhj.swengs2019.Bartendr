652018fbdf34402c7e6f6a4358cdd8f73718ba3e
Sebastian Weiland <hello@sweiland.at>
2018-11-30 23:46:08 +0100

    getters, setter, constructors; and a lot of cheap hacks for now

--- src/main/java/at/bartendr/backend/model/Category.java
+++ src/main/java/at/bartendr/backend/model/Category.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class Category {
@@ -27,4 +29,96 @@ public class Category {
     @Version
     @JsonIgnore
     private long version;
+
+    public Category() {
+    }
+
+    public Category(String name, int reqAge) {
+        this.name = name;
+        this.reqAge = reqAge;
+        // get current date and subtract required age
+        // works only on creation though, should probably be done in frontend tbh
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.YEAR, (reqAge * -1));
+        this.reqBDay = cal.getTime();
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", reqAge=" + reqAge +
+                ", reqBDay=" + reqBDay +
+                ", drinks=" + drinks +
+                ", version=" + version +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Category category = (Category) o;
+        return id == category.id &&
+                reqAge == category.reqAge &&
+                name.equals(category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, reqAge);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReqAge() {
+        return reqAge;
+    }
+
+    public void setReqAge(int reqAge) {
+        this.reqAge = reqAge;
+    }
+
+    public Date getReqBDay() {
+        return reqBDay;
+    }
+
+    public void setReqBDay(Date reqBDay) {
+        this.reqBDay = reqBDay;
+    }
+
+    public List<Drink> getDrinks() {
+        return drinks;
+    }
+
+    public void setDrinks(List<Drink> drinks) {
+        this.drinks = drinks;
+    }
+
+    public long getVersion() {
+        return version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
 }
--- src/main/java/at/bartendr/backend/model/Drink.java
+++ src/main/java/at/bartendr/backend/model/Drink.java
@@ -3,6 +3,7 @@ package at.bartendr.backend.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 public class Drink {
@@ -13,6 +14,7 @@ public class Drink {
 
     private String name;
     private int temperatureCelsius;
+    private int temperatureFahrenheit;
     private boolean isAlcoholic;
     private String color;
     private String character;
@@ -30,4 +32,163 @@ public class Drink {
     @Version
     @JsonIgnore
     private long version;
+
+    public Drink() {
+    }
+
+    // apply same hack as in size with boolean flag for unit
+    public Drink(String name, int temperature, boolean isAlcoholic, String color, String character, String style, boolean unitCelsius) {
+        this.name = name;
+        if (unitCelsius) {
+            this.temperatureCelsius = temperature;
+            this.temperatureFahrenheit = (int) Math.round((temperature * 1.8) + 32);
+        } else {
+            this.temperatureFahrenheit = temperature;
+            this.temperatureCelsius = (int) Math.round((temperature - 32) * 1.8);
+        }
+        this.isAlcoholic = isAlcoholic;
+        this.color = color;
+        this.character = character;
+        this.style = style;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Drink drink = (Drink) o;
+        return id == drink.id &&
+                temperatureCelsius == drink.temperatureCelsius &&
+                temperatureFahrenheit == drink.temperatureFahrenheit &&
+                isAlcoholic == drink.isAlcoholic &&
+                name.equals(drink.name) &&
+                Objects.equals(color, drink.color) &&
+                Objects.equals(character, drink.character) &&
+                Objects.equals(style, drink.style) &&
+                Objects.equals(place, drink.place) &&
+                Objects.equals(size, drink.size) &&
+                Objects.equals(category, drink.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, temperatureCelsius, temperatureFahrenheit, isAlcoholic, color, character, style, place, size, category);
+    }
+
+    public int getTemperatureFahrenheit() {
+        return temperatureFahrenheit;
+    }
+
+    public void setTemperatureFahrenheit(int temperatureFahrenheit) {
+        this.temperatureFahrenheit = temperatureFahrenheit;
+    }
+
+    @Override
+    public String toString() {
+        return "Drink{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", temperatureCelsius=" + temperatureCelsius +
+                ", isAlcoholic=" + isAlcoholic +
+                ", color='" + color + '\'' +
+                ", character='" + character + '\'' +
+                ", style='" + style + '\'' +
+                ", place=" + place +
+                ", size=" + size +
+                ", category=" + category +
+                ", version=" + version +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTemperatureCelsius() {
+        return temperatureCelsius;
+    }
+
+    public void setTemperatureCelsius(int temperatureCelsius) {
+        this.temperatureCelsius = temperatureCelsius;
+    }
+
+    public boolean isAlcoholic() {
+        return isAlcoholic;
+    }
+
+    public void setAlcoholic(boolean alcoholic) {
+        isAlcoholic = alcoholic;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public long getVersion() {
+        return version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
 }
