452a002609a88b95fca828e8e0068d5b484933c6
heiderst16 <stefan.heider@edu.fh-joanneum.at>
2019-01-08 12:13:51 +0100

    Implemented Raw Components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DrinkListComponent } from './drink-list/drink-list.component';
+import { DrinkFormComponent } from './drink-form/drink-form.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { LoginComponent } from './login/login.component';
+import { LocationFormComponent } from './location-form/location-form.component';
+import { CreateDrinkFormComponent } from './create-drink-form/create-drink-form.component';
+import { CreateLocationFormComponent } from './create-location-form/create-location-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrinkListComponent,
+    DrinkFormComponent,
+    UserProfileComponent,
+    LoginComponent,
+    LocationFormComponent,
+    CreateDrinkFormComponent,
+    CreateLocationFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/drink-form/drink-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drink-form',
+  templateUrl: './drink-form.component.html',
+  styleUrls: ['./drink-form.component.scss']
+})
+export class DrinkFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
